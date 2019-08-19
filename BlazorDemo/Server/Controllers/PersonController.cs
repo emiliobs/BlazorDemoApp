726e6cd6cb5127ffdf189017bdc4610eb3a065e1
emilio barrera <barrera_emilio@hotmail.com>
2019-08-19 13:36:47 +0100

    Add project files.

---
+++ BlazorDemo/Server/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemo.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<PersonModel> Getpeople()
+        {
+                      return new List<PersonModel> 
+                      {
+                        new PersonModel{ FirstName = "Emilio", LastName = "Barrera" , AccountBalance = 19.45M },
+                        new PersonModel{ FirstName = "Tim", LastName = "Corey" , AccountBalance = 20.45M },
+                        new PersonModel{ FirstName = "Emiliana", LastName = "Tom" , AccountBalance = 50.45M },
+                        new PersonModel{ FirstName = "Chalsay", LastName = "Waymar" , AccountBalance = 105.45M },
+
+                      };
+
+           
+        }
+
+        [HttpPost]
+         public void Post(PersonModel model)
+        {
+
+        }
+    }
+}
---
+++ BlazorDemo/Shared/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorDemo.Shared
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
